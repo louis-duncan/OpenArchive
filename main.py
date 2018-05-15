d25c16139636f6c69c091b35cf692c5c105af65c
Louis <louis.thurman@me.com>
2018-05-15 19:03:33 +0100

    Added join_files.

--- main.py
+++ main.py
@@ -13,3 +13,34 @@ def main_menu():
     choices = ("Add ")
     while run:
         choice = easygui.buttonbox()
+
+
+def join_files():
+    files = easygui.fileopenbox("Select Files To Combine",
+                                __title__ + " - Combine to PDF",
+                                os.path.join(os.environ["HOMEPATH"],"*.jpg"),
+                                [".jpg", ".pdf"],
+                                True)
+    if files is None:
+        pass
+    else:
+        output_path = easygui.filesavebox("Choose Save Location",
+                                          __title__ + " - Combine to PDF",
+                                          os.path.join(os.environ["HOMEPATH"],"*.pdf"),
+                                          [".pdf"])
+        if output_path is None:
+            pass
+        else:
+            if output_path.endswith(".pdf"):
+                pass
+            else:
+                output_path += ".pdf"
+            err = im2pdf.union(files, output_path)
+            if err:
+                easygui.msgbox('The PDF could not be created!',
+                               __title__ + " - Error")
+            else:
+                pass
+
+
+join_files()
