6fe15995f05076d13a7d02269fa59d712aaa1662
Louis - Desktop <louis.thurman@me.com>
2018-07-16 23:56:54 +0100

    Updates to backup
    - Made backup get dir from database_io

--- backup.py
+++ backup.py
@@ -6,21 +6,20 @@ import shutil
 
 BACKUP_PERIOD = datetime.timedelta(hours=6)
 NUMBER_OF_BACKUPS_TO_RETAIN = 10
-BACKUPS_DIR = os.path.join(os.path.dirname(database_io.DATABASE_LOCATION), "backups")
 
 
 def find_last_backup():
     latest_backup_time = datetime.datetime(1970, 1, 1, 0, 0, 1)
     latest_backup_dir = ""
     try:
-        backup_files = os.listdir(BACKUPS_DIR)
+        backup_files = os.listdir(database_io.BACKUPS_DIR)
     except FileNotFoundError:
         return latest_backup_dir, latest_backup_time
     for f in backup_files:
-        this_files_time = datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(BACKUPS_DIR, f)))
+        this_files_time = datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(database_io.BACKUPS_DIR, f)))
         if this_files_time > latest_backup_time:
             latest_backup_time = this_files_time
-            latest_backup_dir = os.path.join(BACKUPS_DIR, f)
+            latest_backup_dir = os.path.join(database_io.BACKUPS_DIR, f)
         else:
             pass
     return latest_backup_dir, latest_backup_time
@@ -40,23 +39,23 @@ def get_hash(path):
 
 def purge_old_backups():
     backups = []
-    for f in os.listdir(BACKUPS_DIR):
-        backups.append((os.path.getctime(os.path.join(BACKUPS_DIR, f)), f))
+    for f in os.listdir(database_io.BACKUPS_DIR):
+        backups.append((os.path.getctime(os.path.join(database_io.BACKUPS_DIR, f)), f))
     backups.sort()
     for i, b in enumerate(backups):
         if i < NUMBER_OF_BACKUPS_TO_RETAIN:
             pass
         else:
             print("Backup: Purging {}".format(b[1]))
-            os.remove(os.path.join(BACKUPS_DIR, b[1]))
+            os.remove(os.path.join(database_io.BACKUPS_DIR, b[1]))
 
 
 def check_and_backup():
-    if os.path.exists(BACKUPS_DIR):
+    if os.path.exists(database_io.BACKUPS_DIR):
         pass
     else:
-        print("Backup: Creating Backup Dir at {}".format(BACKUPS_DIR))
-        os.mkdir(BACKUPS_DIR)
+        print("Backup: Creating Backup Dir at {}".format(database_io.BACKUPS_DIR))
+        os.mkdir(database_io.BACKUPS_DIR)
     last_backup_dir, last_backup_time = find_last_backup()
     if last_backup_time + BACKUP_PERIOD < datetime.datetime.now():
         bliss = database_io.db_prep_for_access()
@@ -89,9 +88,9 @@ def check_and_backup():
             else:
                 pass
             bliss.connection.close()
-            shutil.copy2(database_io.DATABASE_LOCATION, os.path.join(BACKUPS_DIR, new_backup_filename))
+            shutil.copy2(database_io.DATABASE_LOCATION, os.path.join(database_io.BACKUPS_DIR, new_backup_filename))
             database_io.db_unlock()
-            print("Backup: Back-up created at {}".format(os.path.join(BACKUPS_DIR, new_backup_filename)))
+            print("Backup: Back-up created at {}".format(os.path.join(database_io.BACKUPS_DIR, new_backup_filename)))
     else:
         print("Backup: Not Backing-Up, Backed-up Recently")
     purge_old_backups()
