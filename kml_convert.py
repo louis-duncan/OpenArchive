61f232f126f91611307e9192941067a29ac74579
Whitenoise <39144701+louis-duncan@users.noreply.github.com>
2018-07-16 16:50:15 +0100

    Update KML loading and converting.

--- kml_convert.py
+++ kml_convert.py
@@ -1,121 +1,156 @@
 import os
+import coord
+try:
+    import database_io
+except ModuleNotFoundError:
+    print("Could not load database_io!")
 
 class point:
-	def __init__(self, name="", description="", files=[], longitude=0, latitude=0):
-		self.name = name
-		self.description = description
-		selcf.files = files
-		self.longitude = longitude
-		self.latitude = latitude
-
-		
+    def __init__(self, name="", description="", linked_files=[], longitude=0, latitude=0):
+        self.name = name
+        self.description = description
+        self.linked_files = list(linked_files)
+        self.longitude = longitude
+        self.latitude = latitude
+        
+    def add_link(self, path):
+        self.linked_files.append(path)
+        
 def get_kml_data(path):
-	try:
-		with open(path, "r") as fh:
-			data = fh.readlines()
-	return data
+    with open(path, "r") as fh:
+        data = fh.readlines()
+    return data
 
 
 def decode_description(text):
-	description = ""
-	files = []
-	
-	# Check for tag which Google Earth uses to denote a point with a complex description.
-	if text.startswith("<![CDATA["):
-		pass
-	else:
-		return text, []
-	trimmed = text.remove("<![CDATA[").remove("]]>")
-	
-	in_tag = False
-	tags = []
-	current_tag_text = ""
-	for c in trimmed:
-		if not in_tag:
-			if c == "<"
-				in_tag = True
-				current_tag_text = ""
-			else:
-				description += c
-		else:
-			if c == ">"
-				in_tag = False
-				tags.append(current_tag_text)
-			else:
-				current_tag_text += c
-	description = description.strip()
-	
-	for t in tags:
-		new_path = t.split("src=")[1].strip('"')
-		files.append(new_path
-	
-	return description, files
-	
-	
+    description = ""
+    found_files = []
+    
+    # Check for tag which Google Earth uses to denote a point with a complex description.
+    if text.startswith("<![CDATA["):
+        pass
+    else:
+        return text, []
+    trimmed = text.replace("<![CDATA[", "").replace("]]>", "")
+    
+    in_tag = False
+    tags = []
+    current_tag_text = ""
+    for c in trimmed:
+        if not in_tag:
+            if c == "<":
+                in_tag = True
+                current_tag_text = ""
+            else:
+                description += c
+        else:
+            if c == ">":
+                in_tag = False
+                tags.append(current_tag_text)
+            else:
+                current_tag_text += c
+    description = description.strip()
+    
+    for t in tags:
+        new_path = t.split("src=")[1].strip('"').replace("file://", "").strip("/")
+        found_files.append(new_path)
+        
+    return description, found_files
+    
+    
 def process_points(data_lines):
-	points = []
-	current_point = point()
-	in_name = False
-	in_description = False
-	in_lat_lon = False
-	for line in data_lines:
-		raw_line = line.strip()
-		
-		# Start new points and save finished points.
-		if raw_line == "<Placemark>":
-			current_point = point()
-		elif raw_line == "</Placemark>":
-			points.append(current_point)
-		else:
-			pass
-			
-		# Track which part of the point we are reading.
-		if raw_line.startswith("<name>"):
-			in_name = True
-			raw_line = raw_line.remove("<name>")
-				
-		if raw_line.startswith("<description>"):
-			in_description = True
-			raw_line = raw_line.remove("<description>")
-		
-		if raw_line.startswith("<coordinates>"):
-			in_lat_lon = True
-			raw_line.remove("<coordinates>")
-		
-		# Put the line's data in the right place depending on current area being read.
-		if in_name:
-			if raw_line.endswith("</name>"):
-				in_name = False
-				raw_line = raw_line.remove("</name>")
-			current_point.name += "\n" + raw_line
-			current_point.name = current_point.name.strip()
-		
-		if in_description:
-			if raw_line.endswith("</description>"):
-				in_description = False
-				raw_line = raw_line.remove("</description>")
-			current_point.description += "\n" + raw_line
-			current_point.description = current_point.description.strip()
-		
-		if in_lat_lon:
-			if raw_line.endswith("</coordinates>"):
-				in_lat_lon = False
-				raw_line = raw_line.remove("</coordinates>")
-			current_point.lat, current.lon = normalise(raw_line)
-	
-	print("Found {} points.".format(len(points)))
-	
-	for p in points:
-		new_desc, files = decode_description(p.description)
-		p.description = new_desc
-		for f in files:
-			p.files.append(f)
-			# Check file.
-			if os.path.exists(path):
-				pass
-			else:
-				print("{}: File {} not found!".format(p.name, f))
-	
-	return points
-		
-	
+    points = []
+    current_point = point()
+    in_point = False
+    in_name = False
+    in_description = False
+    in_lat_lon = False
+    for line in data_lines:
+        raw_line = line.strip()
+        
+        # Start new points and save finished points.
+        if raw_line == "<Placemark>":
+            current_point = point()
+            in_point = True
+            in_name = False
+            in_description = False
+            in_lat_lon = False
+        elif raw_line == "</Placemark>":
+            points.append(current_point)
+            in_point = False
+        else:
+            pass
+            
+        # Track which part of the point we are reading.
+        if in_point:
+            if raw_line.startswith("<name>"):
+                in_name = True
+                raw_line = raw_line.replace("<name>", "")
+                    
+            if raw_line.startswith("<description>"):
+                in_description = True
+                raw_line = raw_line.replace("<description>", "")
+            
+            if raw_line.startswith("<coordinates>"):
+                in_lat_lon = True
+                raw_line = raw_line.replace("<coordinates>", "")
+            
+            # Put the line's data in the right place depending on current area being read.
+            if in_name:
+                if raw_line.endswith("</name>"):
+                    in_name = False
+                    raw_line = raw_line.replace("</name>", "")
+                current_point.name += "\n" + raw_line
+                current_point.name = current_point.name.strip()
+            
+            if in_description:
+                if raw_line.endswith("</description>"):
+                    in_description = False
+                    raw_line = raw_line.replace("</description>", "")
+                current_point.description += "\n" + raw_line
+                current_point.description = current_point.description.strip()
+            
+            if in_lat_lon:
+                if raw_line.endswith("</coordinates>"):
+                    in_lat_lon = False
+                    raw_line = raw_line.replace("</coordinates>", "")
+                current_point.longitude, current_point.latitude = coord.normalise(raw_line)
+        else:
+            pass
+    
+    print("Found {} points.".format(len(points)))
+    print("Checking data integrity...")
+
+    error_count = 0
+    for p in range(len(points)):
+        new_desc, point_files = decode_description(points[p].description)
+        points[p].description = new_desc
+        if len(point_files) == 0:
+            print("{}: Has no attached files!".format(points[p].name))
+            error_count += 1
+        else:
+            for f in point_files:
+                points[p].add_link(f)
+                # Check file.
+                if os.path.exists(f):
+                    pass
+                else:
+                    print("{}: File {} not found!".format(points[p].name, f))
+                    error_count += 1
+    if error_count == 0:
+        print("No Errors Found!")
+    else:
+        print(error_count, "Errors Found!")
+    
+    return points
+
+
+def convert_points_to_records(points):
+    new_records = []
+
+    for p in points:
+        pass
+
+
+if __name__ == "__main__":
+    pass                 
